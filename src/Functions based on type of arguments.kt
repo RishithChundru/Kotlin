1606bb8d862189554b010c206ed4c2cbdf97bfaa
Rishith <rishithrishith1@gmail.com>
2024-08-26 12:37:58 +0530

    new files

---
+++ src/Functions based on type of arguments.kt
@@ -0,0 +1,19 @@
+// Default Arguements
+/*fun sum(a: Int =10,b:Int=20){
+    val c=a+b
+    println(c)
+}
+fun main(){
+    sum(20,50)
+    sum()
+}*/
+
+// Without default Arguements
+fun sum(a:Int,b:Int){
+    val c=a+b
+    println(c)
+}
+fun main(){
+    sum(20,50)
+    //sum() // this line will give error
+}
---
+++ src/functions.kt
@@ -0,0 +1,6 @@
+fun demo(){
+    println("Hello world!")
+}
+fun main(args: Array<String>){
+    demo()
+}
